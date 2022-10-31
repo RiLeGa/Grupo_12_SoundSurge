437f5c36ecaa3c2ef6ebdbdfcf056076d4f1969d
Rilega <leandroriosga@gmail.com>
2022-10-31 01:07:26 -0300

    modifico vista de perfil aplico sombras a la credencial

--- site/javascript/carrousel.js
+++
@@ -1,9 +0,0 @@
-new Glider(document.querySelector('.carrousel__container'), {
-    slidesToShow: 1,
-    dots: '#dots',
-    draggable: true,
-    arrows: {
-      prev: '.carousel-anterior',
-      next: '.carousel-siguiente'
-    }
-  });

c44b0dc50a32b9e060986bb0abe47d763d939a5e
Rilega <leandroriosga@gmail.com>
2022-11-23 09:16:46 -0300

    modifico vista en detalle de producto para que se visualicen los productos relacionados

--- public/javascript/busqueda.js
+++ public/javascript/busqueda.js
@@ -1,10 +1,16 @@
+window.addEventListener('load', () => {
 
+    /* let $ = (elemento) => document.querySelector(elemento)
+    console.log("buscador vinculado");
+ */
 document.addEventListener("keyup", e=>{
-    /* console.log(e.target.value); */
+    console.log(e.target.value);
     if(e.target.matches("#inputSearch"))
-    document.querySelectorAll("h3").forEach(producto =>{
-        /* console.log(producto); */
+    document.querySelectorAll(".producto-buscador").forEach(producto =>{
         producto.textContent.toLowerCase().includes(e.target.value)
-        ? producto.classList.remove(".filtro") : producto.classList.add(".filtro")
+        ? producto.classList.remove('filtro') 
+        : producto.classList.add('filtro')
     })
+   
+})
 })

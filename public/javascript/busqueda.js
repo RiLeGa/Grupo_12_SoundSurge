6904e23181c55045004ac9e74dc7042302edddd9
Rilega <leandroriosga@gmail.com>
2022-11-22 23:56:12 -0300

    modifico archivo js de buscador, realizo algo de css en vistas de categorias y marcas

--- public/javascript/busqueda.js
+++ public/javascript/busqueda.js
@@ -1,58 +1,10 @@
 
-
-window.addEventListener('load', () => {
-
-    let vinculacion = 'Documento vinculado con exito'
-    console.log(vinculacion);
-    
-    let $ = (elemento) => document.querySelector(elemento)
-    let selectorAll = (elemento) => document.querySelectorAll(elemento)
-
-    let buscador = $('#inputSearch')
-    let botones = selectorAll('.botonRedireccion')
-    let palabra = ""
-    buscador.onkeydown = (event) => {
-        let letra = event.key
-        letra === 'Backspace' ? palabra = palabra.substring(0, palabra.length - 1) : letra.length > 1 ? null : palabra = palabra += letra
-        if (palabra === 'gibson') {
-            botones.forEach(element => {
-                element.style.backgroundColor = 'var(--colorFondo)'
-            });
-        }
-    }
-    
+document.addEventListener("keyup", e=>{
+    /* console.log(e.target.value); */
+    if(e.target.matches("#inputSearch"))
+    document.querySelectorAll("h3").forEach(producto =>{
+        /* console.log(producto); */
+        producto.textContent.toLowerCase().includes(e.target.value)
+        ? producto.classList.remove(".filtro") : producto.classList.add(".filtro")
+    })
 })
-
-
-/* window.onload = function() {
-
-    let vinculacion = "Documento vinculado con exito"
-    console.log(vinculacion);
-
-document.getElementById("inputSearch").addEventListener("keyup", buscador)
-
-function buscador(){
-    filter = inputSearch.value.toUperCase();
-    li = box_search.getElementByIdTagName("li")
-
-    for(i = 0; i < li.length; i++){
-        
-        a = li[i].getElementByIdTagName("a")[0];
-        textValue = a.textContent || a.innerText;
-
-        if(textValue.toUperCase().indexOf(filter) > -1){
-            li[i].style.display = "";
-            box_search.style.display = "block";
-
-            if(search.value === ""){
-                box_search.style.display = "none";
-            }
-
-        }else{
-            li[i].style.display = "none";
-        }    
-    }
-    
-}
-} */
-

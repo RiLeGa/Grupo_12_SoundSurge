b51182a2a9b3eb97a375e9c676ba2793cec22c37
Rilega <leandroriosga@gmail.com>
2022-11-29 02:40:15 -0300

    creo vista de lo + vendido e implemento buscador a lista de productos en admin

--- site/routes/productos.js
+++ site/routes/productos.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { productos } = require('../controllers/api/apiController');
 const router = express.Router();
 
-const {detalle, carrito, listarCategorias, listarMarcas, listarTodos} = require('../controllers/productosController')
+const {detalle, carrito, listarCategorias, listarMarcas, listarTodos, listarMasVendidos} = require('../controllers/productosController')
 
 router.get('/detalle/:id', detalle);
 router.get('/carrito/:id', carrito);
 router.get('/categorias', listarCategorias);
 router.get('/marcas', listarMarcas);
 router.get('/todos', listarTodos);
+router.get('/loMasVendido', listarMasVendidos);
 
 
 module.exports = router;
